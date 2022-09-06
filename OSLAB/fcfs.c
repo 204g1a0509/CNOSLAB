56c16d1d470027932179304647d653317ae06c4a
204g1a0509 <111738343+204g1a0509@users.noreply.github.com>
2022-09-06 12:29:26 +0530

    commited fcfs

--- OSLAB/fcfs.c
+++ OSLAB/fcfs.c
@@ -1,3 +1,9 @@
+/* Name Of the Candidate: B. Amrutha
+   Roll No: 204G1A0509
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
 #include<stdio.h>
 #include<conio.h>
 main()
